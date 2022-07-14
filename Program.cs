fb809d403ebc2aba6d07bc2a2ad94b18483cec4f
unknown <jorge.tm.ink@outlook.com>
2022-07-14 21:37:53 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Cual es tu nombre");
+string nombre = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Hola " + nombre );
+
+Console.WriteLine("¿Que hora es?");
+int hora = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("¿Con cuantos minutos?");
+int minutos = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Son las " + hora + ":" + minutos);
